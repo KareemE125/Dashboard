2d3b41e6bad2f0fbed728e8f00371f79c590fe4a
KareemE125 <kareemezzat.125@gmail.com>
2022-12-08 23:29:53 +0200

    Performance + bug fixes

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs'
 import { Header } from '../components'
 
 
 export default function ColorPicker() 
 {
-  const pen = document.getElementById('preview');
-  function changePenColor(color){ pen.style.backgroundColor = color.currentValue.hex; }
+  const penRef = useRef()
+  
+  function changePenColor(color){ 
+    penRef.current.style.backgroundColor = color.currentValue.hex;
+  }
 
   return <main className='bg-white rounded-3xl'>
     <Header title='Color Picker' category='App' />
     <section className='text-center'>
-      <div id='preview' />
+      <div ref={penRef} id='preview' />
       <div className="flex flex-wrap justify-center items-center gap-20">
         <div>
           <h4 className='text-2xl font-semibold p-4'>Inline Pallette</h4>
