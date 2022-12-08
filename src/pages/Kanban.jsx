513b43043936705aa1d4edfd88fccbbc57ad6a20
KareemE125 <kareemezzat.125@gmail.com>
2022-12-08 17:20:51 +0200

    Kanban Tasks DONE 📑

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban'
+import { kanbanData, kanbanGrid } from '../data/dummy'
+import { Header } from '../components'
+
 export default function Kanban() {
-  return (
-    <div>Kanban</div>
-  )
+  return <main className='bg-white rounded-3xl'>
+    <Header title='Kanban' category='App' />
+    <KanbanComponent
+      id='kanban'
+      dataSource={kanbanData}
+      cardSettings={{contentField:'Summary',headerField:'Id'}}
+      keyField='Status'
+    >
+      <ColumnsDirective>
+        {
+          kanbanGrid.map((item) => <ColumnDirective key={item.keyField} {...item} />)
+        }
+      </ColumnsDirective>
+    </KanbanComponent>
+  </main>
 }
