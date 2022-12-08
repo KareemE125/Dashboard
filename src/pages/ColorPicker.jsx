a80f7d19d1410a8bb1510ee145e78f1e0cf97b08
KareemE125 <kareemezzat.125@gmail.com>
2022-12-08 18:35:08 +0200

    Color Picker

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
-export default function ColorPicker() {
-  return (
-    <div>ColorPicker</div>
-  )
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs'
+
+import { Header } from '../components'
+
+
+
+export default function ColorPicker() 
+{
+
+  const pen = document.getElementById('preview');
+  function changePenColor(color){ pen.style.backgroundColor = color.currentValue.hex; }
+
+  return <main className='bg-white rounded-3xl'>
+    <Header title='Color Picker' category='App' />
+    <section className='text-center'>
+      <div id='preview' />
+      <div className="flex flex-wrap justify-center items-center gap-20">
+        <div>
+          <h4 className='text-2xl font-semibold p-4'>Inline Pallette</h4>
+          <ColorPickerComponent id='inline-palette' mode='Palette' modeSwitcher={false} inline showButtons={false} change={changePenColor} />
+        </div>
+        <div>
+          <h4 className='text-2xl font-semibold p-4'>Inline Picker</h4>
+          <ColorPickerComponent id="inline-picker" mode="Picker" modeSwitcher={false} inline showButtons={false} change={changePenColor}/>
+        </div>
+      </div>
+    </section>
+  </main>
 }
